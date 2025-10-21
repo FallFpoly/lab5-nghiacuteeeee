270f01a70a1323097f730d4867e69ecbcdb6db39
Admin <Admin@chotaoxin500k.lan>
2025-10-21 21:27:32 +0700

    nop lap4.com

--- Lab5/src/main/java/com/mycompany/lab5/bai4.java
+++ Lab5/src/main/java/com/mycompany/lab5/bai4.java
@@ -11,6 +11,6 @@ package com.mycompany.lab5;
 public class bai4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("xin chao the gioi");
     }
 }
